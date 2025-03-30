ece9b54bc1606895eec7391ec613668f40fd6f5e
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2025-03-30 21:54:08 +0600

    Dropdowns

--- src/app/page/user-list/user-list.component.ts
+++ src/app/page/user-list/user-list.component.ts
@@ -1,10 +1,11 @@
 import { DatePipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-list',
-  imports: [DatePipe],
+  imports: [DatePipe, FormsModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
@@ -13,9 +14,17 @@ export class UserListComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   userList : any = [];
+  stateList : any = [];
+  districtList : any = [];
+  cityList : any = [];
+
+  selectedSateId: string = '';
+  selectedDistrictId: string = '';
+  selectedCity: string = '';
 
   ngOnInit(): void {
     this.getUsers();
+    this.getAllState();
   }
 
   getUsers() {
@@ -24,4 +33,23 @@ export class UserListComponent implements OnInit {
     });
   }
 
+  getAllState(){
+    this.http.get('https://localhost:7276/api/Master/GetAllState').subscribe((response: any) => {
+      this.stateList = response;
+    }
+    );
+  }
+
+  GetDistrictByStateId() {
+    this.cityList = [];
+    this.http.get('https://localhost:7276/api/Master/GetDistrictByStateId?stateId=' + this.selectedSateId).subscribe((response: any) => {
+      this.districtList = response;
+    });
+  }
+  
+  GetCityByDistrictId() {
+    this.http.get('https://localhost:7276/api/Master/GetCityByDistrictId?districtId=' + this.selectedDistrictId).subscribe((response: any) => {
+      this.cityList = response;
+    });
+  }
 }
