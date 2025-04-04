44f8c45061f8ba9d14139dfe975005e38b14eed0
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2025-04-04 23:52:18 +0600

    Employee List

--- src/app/page/employee-list/employee-list.component.ts
+++ src/app/page/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-employee-list',
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit {
 
+  http =  inject(HttpClient);
+
+  employeeList: any = [];
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7276/api/Employee/GetAllEmployees').subscribe((data: any) => {
+      this.employeeList = data;
+    });
+  }
+  
 }
